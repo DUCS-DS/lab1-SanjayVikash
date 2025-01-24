fb383b17f497d30787d089509e421e7f223eb2a1
SanjayVikash <sanjayvikashvasidhar@gmail.com>
2025-01-24 17:13:54 +0000

    Code runs and gives 1936

--- edit_me.py
+++ edit_me.py
@@ -3,13 +3,14 @@ def find_max(lst):
     """return the maximum element"""
 
     current_max = lst[0]
+    for number in lst :
+        if number > current_max :
+            current_max = number
 
-    #
-    #  YOUR CODE GOES HERE
-    #
+    
 
     return current_max
 
 test_list = [2112*i % 2024 for i in range(10000)]
-
+print("apple")
 print(find_max(test_list))
